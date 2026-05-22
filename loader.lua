2f3d2f5e405bdb22ae4dcd7edfd54ecbeb6cefd7
ZhangJunZ84 <godnebulous666@gmail.com>
2026-05-22 04:13:01 -0300

    update: scripts

--- loader.lua
+++ loader.lua
@@ -8,6 +8,7 @@ local GAMES = {
     [9186719164]  = "sailorpiece.lua",
     [6718454990]  = "animeghost.lua",
     [10032271327] = "worldfighters.lua",
+    [9792947201] = "slimerng.lua"
 }
 
 local gameId = game.GameId
